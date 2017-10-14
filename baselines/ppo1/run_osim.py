5be8a94c07cafd981ff288efedaac39991fbffc4
Shawn Lewis <shlewis@gmail.com>
2017-10-14 16:36:33 -0700

    Add reward shaping options.

--- baselines/ppo1/run_osim.py
+++ baselines/ppo1/run_osim.py
@@ -13,8 +13,12 @@ from osim.env import RunEnv
 import numpy as np
 
 class FixedRunEnv(RunEnv):
-    def __init__(self, visualize=False, difficulty=2):
+    def __init__(self, visualize=False, difficulty=2,
+            fall_smoothing_exp=2, fall_smoothing_max_penalty=0, velocity_penalty_mult=0):
         self._saved_difficulty = difficulty
+        self._fall_smoothing_exp = fall_smoothing_exp
+        self._fall_smoothing_max_penalty = fall_smoothing_max_penalty
+        self._velocity_penalty_mult = velocity_penalty_mult
         super(FixedRunEnv, self).__init__(visualize=visualize)
 
     def seed(self, seed):
@@ -27,6 +31,24 @@ class FixedRunEnv(RunEnv):
 
     def get_observation(self):
         return np.array(super(FixedRunEnv, self).get_observation())
+
+    def step(self, action):
+        next_obs, reward, done, info = super(FixedRunEnv, self).step(action)
+
+        # Penalize for pelvis height getting close to 0.65.
+        pelvis_y = self.current_state[self.STATE_PELVIS_Y]
+        if pelvis_y < 1:
+            # Quadratically go from MAX_PENALTY at pelvis_y=0.65 (simulation ends) to
+            # 0 at pelvis_y=1.0 which is the max
+            # Setting EXPONENT to 1 makes this linear
+            reward -= self._fall_smoothing_max_penalty * (-(pelvis_y - 1)/.35) ** self._fall_smoothing_exp
+
+        # Penalyze fast moving body parts
+        velocities = np.array(self.current_state[-19:-5]) - np.array(self.last_state[-19:-5])
+        vel_squared_sum = np.sum(np.square(velocities))
+        reward -= self._velocity_penalty_mult * vel_squared_sum
+
+        return next_obs, reward, done, info
 
 def train():
     from baselines.ppo1 import mlp_policy, pposgd_simple
@@ -49,7 +71,12 @@ def train():
     workerseed = config['seed'] + 1000000 * MPI.COMM_WORLD.Get_rank()
     set_global_seeds(workerseed)
 
-    env = FixedRunEnv(visualize=config['visualize'])
+    env = FixedRunEnv(
+        visualize=config['visualize'],
+        difficulty=config['difficulty'],
+        fall_smoothing_exp=config['fall_smoothing_exp'],
+        fall_smoothing_max_penalty=config['fall_smoothing_max_penalty'],
+        velocity_penalty_mult=config['velocity_penalty_mult'])
 
     def policy_fn(name, ob_space, ac_space):
         return mlp_policy.MlpPolicy(name=name, ob_space=ob_space, ac_space=ac_space,
